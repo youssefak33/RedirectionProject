e5ce1bf11e64a26d7f9632173ce2783ed52f1c97
Youssef <youssef.akhtab@outlook.com>
2024-02-21 07:53:21 +0100

    ajouter connexion et création de compte front

--- templates/login.php
+++ templates/login.php
@@ -1,5 +1,7 @@
 <?php $title="Page de connexion"?>
 <?php ob_start(); ?>
+    <h2>Se connecter à Trackdirect</h2> 
+
     <form action="index.php?action=connexion" method="post">
         <label for="email">E-mail</label>
         <input type="text" id="email" name="email" maxlength="535" required autocomplete="off">
@@ -8,6 +10,7 @@
         <input type="password" id="pass" name="password" minlength="8" required />
         
         <button type="submit">Se connecter</button>
+        <a href='#'>Créer mon compte</a>
     </form>
 <?php $content = ob_get_clean(); ?>
-<?php require('templates/layout.php') ?>
+<?php require_once('templates/layout.php') ?>
