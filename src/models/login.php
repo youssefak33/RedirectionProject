532e0d8dca92dd060ddfd1130993ec1dc086db3e
Youssef <youssef.akhtab@outlook.com>
2024-02-24 22:48:10 +0100

    User connection database

---
+++ src/models/login.php
@@ -0,0 +1,3 @@
+<?php 
+// require $database
+// connect to the $database to check if the email and mdp are correct and send true function
--- templates/script.js
+++ templates/script.js
@@ -1,13 +1,13 @@
-  function validateForm() {
-    var password = document.getElementById("motDePasse").value;
-    var confirmPassword = document.getElementById("confirmerMotDePasse").value;
-    var errorMessage = document.getElementById("error-message");
+function validateForm() {
+var password = document.getElementById("signup_pass").value;
+var confirmPassword = document.getElementById("confirm_signup_pass").value;
+var errorMessage = document.getElementById("error-message");
 
-    if (password !== confirmPassword) {
-      errorMessage.innerHTML = "Les mots de passe ne correspondent pas.";
-      return false;
-    } else {
-      errorMessage.innerHTML = "";
-      return true;
-    }
-  }
+if (password !== confirmPassword) {
+    errorMessage.innerHTML = "Les mots de passe ne correspondent pas.";
+    return false;
+} else {
+    errorMessage.innerHTML = "";
+    return true;
+}
+}
